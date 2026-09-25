24223621b80b907dcc164808e71fc73a9519eef6
gabrielbalenton <gabrielbalenton@gmail.com>
2026-09-25 15:56:36 +0800

    Route new Robby email signature independently from Daniel

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ const Pitch = lazy(() => import('./pages/Pitch'));
 const FPXStocklist = lazy(() => import('./pages/FPXStocklist'));
 const FPXStocklistLive = lazy(() => import('./pages/FPXStocklistLive'));
 const DMRSignature = lazy(() => import('./pages/DMRSignature'));
+const DMRSignatureRobby = lazy(() => import('./pages/DMRSignatureRobby'));
 const NotFound = lazy(() => import('./pages/NotFound'));
 
 const PageLoader = () => <div className="min-h-screen bg-[#09090B]" />;
@@ -62,6 +63,7 @@ export default function App() {
           <Route path="/pitch/:slug" element={<Pitch />} />
           <Route path="/fpx/stocklist" element={<FPXStocklistLive />} />
           <Route path="/dmr-signature" element={<DMRSignature />} />
+          <Route path="/dmr-signature-robby" element={<DMRSignatureRobby />} />
           <Route path="/fpx/stocklist-classic" element={<FPXStocklist />} />
           <Route path="/fpx/stocklist-beta" element={<Navigate to="/fpx/stocklist" replace />} />
         </Routes>
